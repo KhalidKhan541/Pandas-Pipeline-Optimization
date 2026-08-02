b87d5c91078f964125e92ca8bd2b65d284d61e8e
KhalidKhan541 <khalidkhan46572@gmail.com>
2026-08-02 21:26:12 +0500

    feat: add package init

---
+++ pipeline_optimization/__init__.py
@@ -0,0 +1,7 @@
+"""Large-Scale Pandas Pipeline Optimization.
+
+Systematic optimization of a deliberately slow 50M-row pipeline:
+dtype downcasting, vectorization, chunked I/O, and Dask parallelism.
+"""
+
+__version__ = "1.0.0"
